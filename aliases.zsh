204bed6600c9131d0c88580de67aa3954343732e
Rihards <48697281+RihardsJ@users.noreply.github.com>
2022-11-01 22:01:08 +0000

    Update and rename .aliases.zsh to aliases.zsh

---
+++ aliases.zsh
@@ -0,0 +1,19 @@
+# navigation 
+alias ..="cd .."
+alias work="cd $HOME/Workspace"
+alias doc="cd $HOME/Documents"
+alias dow="cd $HOME/Downloads"
+  
+# list files and folders 
+alias ls="exa"
+alias tree="exa --tree"
+
+# make, copy and move files and folders
+alias mkdir="mkdir -p"
+alias cp="cp -i"
+alias mv="mv -i"
+alias trash="rm -rf"
+
+# mac like clipboard manipulations
+alias pbcopy='xclip -selection clipboard'
+alias pbpaste='xclip -selection clipboard -o'
