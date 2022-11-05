5df9e51d9e6cec5259d2cf11bf7321abb7e91321
Rihards <48697281+RihardsJ@users.noreply.github.com>
2022-11-05 21:44:56 +0000

    Update aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -1,9 +1,20 @@
 # navigation 
 alias ..="cd .."
+
+# list files every time directory is changed
+function cl {
+  if [ "$#" -eq 0 ]; then
+    "cd" || return
+  else
+    "cd" "$1" || return
+  fi
+  "exa"
+
+# common directories
 alias work="cd $HOME/Workspace"
 alias doc="cd $HOME/Documents"
 alias dow="cd $HOME/Downloads"
-  
+
 # list files and folders 
 alias ls="exa"
 alias tree="exa --tree"
